0994179761995caa5e41b64512c8d7cc568616c4
Paolo <paolo.herrera.araya@gmail.com>
2024-09-12 00:19:07 -0300

    margin top 10

--- frontend/src/components/LoadingIndicator.jsx
+++ frontend/src/components/LoadingIndicator.jsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function LoadingIndicator () {
   return (
-    <div className="loading-indicator">
+    <div className="loading-indicator mt-10">
       <p>Generating short URL, please wait...</p>
       <FontAwesomeIcon icon={faCircleNotch} className='flex animate-spin primary-color fa-xl'/>
     </div>
