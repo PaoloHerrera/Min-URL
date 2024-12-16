7dfabdda7565724aad2a491a7285d2fab3a92c74
Paolo <paolo.herrera.araya@gmail.com>
2024-12-15 21:51:54 -0300

    Se utiliza validator para validar las urls

--- backend/middleware/validateUrl.js
+++ backend/middleware/validateUrl.js
@@ -1,16 +1,16 @@
+import validator from 'validator'
+import { addHttpScheme } from '../utils/utils.js'
+
 export const validateUrl = (req, res, next) => {
-  const pattern = new RegExp(
-    '^([a-zA-Z]+:\\/\\/)?' + // protocol
-      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
-      '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR IP (v4) address
-      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
-      '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-      '(\\#[-a-z\\d_]*)?$', // fragment locator
-    'i'
-  )
-  if (pattern.test(req.body.originalUrl)) {
+  let { originalUrl } = req.body
+
+  // Si falta el protocolo se añade
+  originalUrl = addHttpScheme(originalUrl)
+
+  if (validator.isURL(originalUrl, { require_protocol: true })) {
+    req.body.originalUrl = originalUrl
     next()
   } else {
-    res.status(404).json('La url es inválida')
+    res.status(404).json({ error: 'La URL es inválida' })
   }
 }
