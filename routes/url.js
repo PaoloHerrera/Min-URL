6af22e55b42c6b494977cc02a1e886c2bfdd97b5
Paolo <paolo.herrera.araya@gmail.com>
2024-12-19 15:57:39 -0300

    deshabilita trust proxy

--- routes/url.js
+++ routes/url.js
@@ -11,7 +11,7 @@ const routesUrl = Router()
 const generalLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100,
-  trustproxy: false,
+  trustProxy: false,
   message: 'Too many requests from this IP, please try again after 15 minutes.'
 })
 
