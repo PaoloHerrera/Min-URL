40a4306b3159a29fb772ae731615605a112c5db9
Paolo <paolo.herrera.araya@gmail.com>
2024-12-20 00:51:19 -0300

    Se crea el codigo qr con la short url

--- src/components/QRCodeDisplay.jsx
+++ src/components/QRCodeDisplay.jsx
@@ -1,11 +1,12 @@
 import QRCode from 'react-qr-code'
-import { useUrl } from '@hooks/useUrl'
+import { useShortUrl } from '@hooks/useShortUrl'
 
 export function QRCodeDisplay ({ qrCodeRef }) {
-  const { url } = useUrl()
+  const { shortUrl } = useShortUrl()
+
   return (
     <div className='mb-5 ml-5 col-span-3 sm:col-span-4' ref={qrCodeRef}>
-      <QRCode value={url} size={130} />
+      <QRCode value={shortUrl} size={130} />
     </div>
   )
 }
