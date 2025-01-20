4d644713bbc09c83052f2aff482197df690cfe61
Paolo <paolo.herrera.araya@gmail.com>
2025-01-20 01:12:23 -0300

    Se crean las constantes de como funciona min url

--- src/constants.js
+++ src/constants.js
@@ -10,3 +10,26 @@ const PROD_CONSTANTS = {
 
 export const { SHORT_URL_API, RECAPTCHA_SITE_KEY } =
 	process.env.NODE_ENV === 'development' ? DEV_CONSTANTS : PROD_CONSTANTS
+
+export const HOW_MIN_URL_IT_WORKS = [
+	{
+		id: 1,
+		title: '1. Enter the URL of the website you want to minify',
+		image: '/public/01.webp',
+	},
+	{
+		id: 2,
+		title: '2. Click the "Minify URL" button',
+		image: '/public/02.webp',
+	},
+	{
+		id: 3,
+		title: '3. Wait a few seconds for the magic to happen',
+		image: '/public/03.webp',
+	},
+	{
+		id: 4,
+		title: '4. Done! You have a new minified URL and QR Code. Share it!',
+		image: '/public/04.webp',
+	},
+]
