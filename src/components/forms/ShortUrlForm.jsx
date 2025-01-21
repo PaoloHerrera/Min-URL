7b9e599a8b007e40164eb35bc1c198062db03a04
Paolo <paolo.herrera.araya@gmail.com>
2025-01-20 21:39:35 -0300

    Se agrega carga dinámica de reCAPTCHA en el formulario de URL corta

--- src/components/forms/ShortUrlForm.jsx
+++ src/components/forms/ShortUrlForm.jsx
@@ -1,15 +1,17 @@
-import { Form } from "@heroui/react"
+import { Form } from '@heroui/react'
 import { useUrl } from '@hooks/useUrl'
 import { useShortUrl } from '@hooks/useShortUrl'
-import { useCallback } from 'react'
+import { useCallback, useState } from 'react'
 import debounce from 'just-debounce-it'
 import InputUrl from '@components/InputUrl'
 import SubmitButton from '@components/SubmitButton'
 import ErrorMessage from '@components/ErrorMessage'
+import { RECAPTCHA_SITE_KEY } from '../../constants'
 
 export function ShortUrlForm() {
 	const { url, setInvalid } = useUrl()
 	const { getShortUrl } = useShortUrl()
+	const [isRecaptchaLoaded, setRecaptchaLoaded] = useState(false)
 
 	const debouncedGetShortUrl = useCallback(
 		debounce((url) => {
@@ -18,11 +20,32 @@ export function ShortUrlForm() {
 		[],
 	)
 
+	const loadRecaptcha = () => {
+		const script = document.createElement('script')
+		script.src = `https://www.google.com/recaptcha/api.js?render=${RECAPTCHA_SITE_KEY}`
+		script.async = true
+		document.head.appendChild(script)
+
+		return script
+	}
+
 	const handleSubmit = async (e) => {
 		e.preventDefault()
 		const isValid = !setInvalid(url)
 		if (!isValid) return
-		debouncedGetShortUrl(url)
+
+		if (!isRecaptchaLoaded) {
+			const script = loadRecaptcha()
+
+			script.onload = () => {
+				setRecaptchaLoaded(true)
+
+				debouncedGetShortUrl(url)
+			}
+			document.head.appendChild(script)
+		} else {
+			debouncedGetShortUrl(url)
+		}
 	}
 
 	return (
