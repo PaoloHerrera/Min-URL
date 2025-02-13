2997334746920b8c399f12ae6e63c2792e6177fe
Paolo <paolo.herrera.araya@gmail.com>
2025-02-13 18:37:32 -0300

    Se refactoriza el controlador de URL para mejorar la generación de slugs y la gestión de errores en la creación de Short URLs

--- controllers/UrlController.js
+++ controllers/UrlController.js
@@ -1,153 +1,145 @@
 import crypto from 'node:crypto'
-import geoip from 'geoip-lite'
 import { UrlModel } from '../models/Url.js'
 import { LogUrlModel } from '../models/LogUrl.js'
 import { validateUrl } from '../schema/url.js'
 import { validateLogUrl } from '../schema/logurl.js'
-import { addHttpScheme } from '../utils/utils.js'
+import { base64ToBase62 } from '../utils/utils.js'
 import { SHORTURL_VALUES } from '../constants.js'
 
-// Función para obtener geolocalización
-const getGeoLocation = (ip) => geoip.lookup(ip) || {}
+const ERROR_MESSAGE = 'Error creating Short URL. Please try again later.'
 
-// Esta función permite crear una ShortUrl con intentos
-const generateShortUrl = async (longUrl, length, maxLength, maxAttempts) => {
-	let attempts = 0
-	let initialLength = length
-	const normalizedUrl = addHttpScheme(longUrl)
+const generateHash = (input) => {
+	return crypto.createHash('sha256').update(input).digest('base64')
+}
 
-	while (length <= maxLength) {
-		const inputForShortUrl = `${normalizedUrl}-${crypto.randomUUID()}`
+const validateSlugLength = (slug, length) => {
+	if (slug.length < length) {
+		throw new Error(ERROR_MESSAGE)
+	}
+}
 
-		//Se genera el hash de la url
-		const hash = crypto
-			.createHash('sha256')
-			.update(inputForShortUrl)
-			.digest('base64url')
-		const shorturl = hash.substring(0, length)
+class SlugGenerator {
+	constructor(config) {
+		this.config = config
+	}
 
-		//Se verifica si la shorturl ya existe. Si no existe se devuelve la shorturl. Si ya existe se vuelve a intentar con un nuevo hash
+	// Esta función permite crear una ShortUrl con intentos
+	// Recibe una URL y devuelve una promise que se resuelve con el slug
+	// Si no se resuelve, se devuelve un error
+	async generateUniqueSlug(longUrl) {
+		let attempts = 0
+		const { maxLength, maxAttempts } = this.config
+		let currentLength = this.config.initialLength
+
+		while (currentLength <= maxLength) {
+			const slug = await this.generateSlugAttempts(longUrl, currentLength)
+			if (slug) return slug
+
+			attempts = this.handleFailedAttempts(attempts, maxAttempts)
+			currentLength = this.adjustLength(attempts, currentLength)
+		}
+
+		throw new Error(ERROR_MESSAGE)
+	}
+
+	// Esta función crea un slug basado en la URL y el largo que se quiere asignar
+	// Recibe una URL y un largo y devuelve una promise que se resuelve con el slug
+	// Si no se resuelve, se devuelve un error
+	async generateSlugAttempts(longUrl, length) {
 		try {
-			const exists = await UrlModel.findOne({ where: { shorturl } })
-			if (!exists) return shorturl
+			const inputForSlug = `${longUrl}-${crypto.randomUUID()}`
+			const base62Hash = base64ToBase62(generateHash(inputForSlug))
+			validateSlugLength(base62Hash, length)
+			const slug = base62Hash.substring(0, length)
+			return (await this.isSlugAvailable(slug)) ? slug : ''
 		} catch (error) {
-			throw new Error('Error creating Short URL. Please try again later.')
+			throw new Error(ERROR_MESSAGE)
 		}
-		attempts++
+	}
 
+	//Verifica si el slug ya existe
+	async isSlugAvailable(slug) {
+		try {
+			const exists = await UrlModel.findOne({ where: { slug } })
+			return !exists
+		} catch (error) {
+			throw new Error(ERROR_MESSAGE)
+		}
+	}
+
+	async handleFailedAttempts(attempts, maxAttempts) {
 		if (attempts >= maxAttempts) {
-			initialLength++
-			attempts = 0
+			return 0
 		}
+		return attempts + 1
 	}
 
-	throw new Error('Error creating Short URL. Please try again later.')
-}
-
-export const createShortUrl = async (req, res) => {
-	// Capturamos la ip del usuario
-	let ip = req.ip
-
-	const initialLength = SHORTURL_VALUES.initialLength
-	const maxAttempts = SHORTURL_VALUES.maxAttempts
-	const longUrl = req.body.originalUrl
-	const maxLength = SHORTURL_VALUES.maxLength
-	let resultUrl
-
-	if (ip === '::1' || ip === '::ffff:127.0.0.1') ip = process.env.LOCAL_IP
-
-	const geo = getGeoLocation(ip)
-
-	console.log('geo: ', geo)
-	console.log('ip: ', ip)
-
-	// Verifica si geo es vacío
-	if (Object.keys(geo).length === 0) {
-		return res.status(400).json({
-			message:
-				"We couldn't generate your Short URL at this time. Please try again later.",
-		})
-	}
-
-	try {
-		//Generamos la shorturl
-		const shorturl = await generateShortUrl(
-			longUrl,
-			initialLength,
-			maxLength,
-			maxAttempts,
-		)
-
-		// Usuario anónimo.
-		// TODO: Incorporar usuario registrado
-		const objectUrl = {
-			longurl: addHttpScheme(longUrl),
-			id_url_hash: crypto.randomUUID(),
-			shorturl,
-			ip_address: ip,
-			country: geo.country,
-			region: geo.region,
-			timezone: geo.timezone,
-			city: geo.city,
-			latitude: geo.ll[0],
-			longitude: geo.ll[1],
-		}
-
-		resultUrl = validateUrl(objectUrl)
-
-		if (!resultUrl.success) {
-			throw new Error(resultUrl.error.message)
-		}
-		await UrlModel.create(resultUrl.data)
-		return `${req.protocol}://${req.get('host')}/${resultUrl.data.shorturl}`
-	} catch (error) {
-		console.log('Error al crear una nueva url: ', error.message)
-		throw new Error('Error creating Short URL. Please try again later.')
+	async adjustLength(attempts, currentLength) {
+		attempts === 0 ? currentLength + 1 : currentLength
+		return currentLength
 	}
 }
+
+const createUrlObject = (req, slug) => ({
+	longurl: req.body.originalUrl,
+	id_url_hash: crypto.randomUUID(),
+	slug,
+	purpose: req.purpose,
+	ip_address: req.originalIp,
+	...extractGeoData(req.geo),
+})
+
+const createLogUrlObject = (req, url) => ({
+	url_id_url: url.dataValues.id_url,
+	id_logurl_hash: crypto.randomUUID(),
+	ip_address: req.originalIp,
+	...extractGeoData(req.geo),
+})
+
+const extractGeoData = (geo) => ({
+	country: geo.country,
+	region: geo.region,
+	timezone: geo.timezone,
+	city: geo.city,
+	latitude: geo.ll[0],
+	longitude: geo.ll[1],
+})
+
+const saveUrl = async (data) => {
+	const result = validateUrl(data)
+	if (!result.success) {
+		throw new Error(result.error.message)
+	}
+	return UrlModel.create(result.data)
+}
+
+export const createShortUrl = async (req, res, next) => {
+	const generator = new SlugGenerator({
+		initialLength: SHORTURL_VALUES.initialLength,
+		maxAttempts: SHORTURL_VALUES.maxAttempts,
+		maxLength: SHORTURL_VALUES.maxLength,
+	})
+
+	const slug = await generator.generateUniqueSlug(req.body.originalUrl)
+	const urlData = createUrlObject(req, slug)
+
+	await saveUrl(urlData)
+
+	req.shorturl = `${req.protocol}://${req.get('host')}/${slug}`
+	req.slug = slug
+	next()
+}
+
+export const sendShortUrl = (req, res) => res.send(req.shorturl)
 
 export const getShortUrl = async (req, res) => {
-	const shorturl = req.params.id
+	const { slug } = req.params
+	const url = await UrlModel.findOne({ where: { slug } })
 
-	// Capturamos la ip del usuario
-	let ip = req.ip
+	if (!url) return res.status(404).json({ message: 'Short URL not found' })
 
-	if (ip === '::1') ip = process.env.LOCAL_IP
-	const geo = getGeoLocation(ip)
+	await url.increment({ clicks: 1 })
+	await LogUrlModel.create(validateLogUrl(createLogUrlObject(req, url)).data)
 
-	// Se consulta en la base de datos si existe la shorturl creada
-	try {
-		const url = await UrlModel.findOne({ where: { shorturl } })
-
-		if (!url) return res.status(404).json({ message: 'Short URL not found' })
-
-		// Si la url fue encontrada se actualiza la cantidad de clicks. Sumando +1
-		await url.increment({ clicks: 1 })
-
-		const objectLogUrl = {
-			url_id_url: url.dataValues.id_url,
-			id_logurl_hash: crypto.randomUUID(),
-			ip_address: ip,
-			country: geo.country,
-			region: geo.region,
-			timezone: geo.timezone,
-			city: geo.city,
-			latitude: geo.ll[0],
-			longitude: geo.ll[1],
-		}
-
-		const resultLogUrl = validateLogUrl(objectLogUrl)
-
-		if (!resultLogUrl.success) {
-			return res.status(400).json({ error: resultLogUrl.error.message })
-		}
-
-		// Inserta el Log en la base de datos
-		await LogUrlModel.create(resultLogUrl.data)
-
-		res.redirect(url.longurl)
-	} catch (error) {
-		console.log('Error al leer la url: ', error.message)
-		return res.status(400).json({ error: error.message })
-	}
+	res.redirect(url.longurl)
 }
