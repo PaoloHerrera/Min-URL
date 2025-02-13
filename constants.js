8ebc3b89d43a1ba15d4a73232cadc19c80243583
Paolo <paolo.herrera.araya@gmail.com>
2025-02-13 18:33:19 -0300

    Ajustar constantes: aumentar longitudes de short URL y límite de códigos QR por día en constants.js

--- constants.js
+++ constants.js
@@ -1,9 +1,10 @@
 export const SHORTURL_VALUES = {
 	initialLength: 6,
 	maxAttempts: 20,
-	maxLength: 10,
+	maxLength: 12,
 }
 
 export const LIMITS_VALUES = {
 	limitShortUrlPerDay: 10,
+	limitQRCodePerDay: 5,
 }
