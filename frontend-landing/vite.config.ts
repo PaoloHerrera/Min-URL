09bc741f371f2835f0408b85d5596b9c5b1d2ec2
Paolo <paolo.herrera.araya@gmail.com>
2025-03-05 03:40:55 -0300

    Configura el puerto del servidor a 4000 en vite.config.ts para el proyecto de frontend-landing

--- frontend-landing/vite.config.ts
+++ frontend-landing/vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+	server: {
+		port: 4000,
+	},
 	plugins: [react(), tailwindcss()],
 	resolve: {
 		alias: {
