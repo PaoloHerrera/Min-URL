e06b136d9b04a55266c98ecf20489ba42dd4b1ce
Paolo <paolo.herrera.araya@gmail.com>
2025-03-07 03:00:03 -0300

    Configura CORS en el servidor de backend para permitir solicitudes desde la URL del dashboard y habilitar credenciales

--- backend-users/src/main.ts
+++ backend-users/src/main.ts
@@ -5,6 +5,10 @@ import cookieParser from 'cookie-parser'
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 	app.use(cookieParser())
+	app.enableCors({
+		origin: [process.env.DASHBOARD_URL],
+		credentials: true,
+	})
 	const port = process.env.PORT || 3000
 	console.log(`Server running on port ${port}`)
 	await app.listen(port)
