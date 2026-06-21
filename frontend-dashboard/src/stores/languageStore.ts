69d57fa102eeb94cef4530907135488692920f4d
Paolo Herrera <paolo.herrera.araya@gmail.com>
2026-06-21 01:31:04 -0400

    feat: add language query parameters to dashboard links and improve language initialization logic (#4)

--- frontend-dashboard/src/stores/languageStore.ts
+++ frontend-dashboard/src/stores/languageStore.ts
@@ -6,8 +6,34 @@ type LanguageStore = {
 	setLanguage: (language: Language) => void
 }
 
+const getInitialLanguage = (): Language => {
+	//1. Get Query Params
+	const queryParams = new URLSearchParams(window.location.search)
+	const lang = queryParams.get('lang') as Language | null
+	if (lang && ['en', 'es'].includes(lang)) {
+		localStorage.setItem('language', lang)
+		return lang as Language
+	}
+
+	//2. Get Local Storage
+	const langLocalStorage = localStorage.getItem('language') as Language | null
+	if (langLocalStorage && ['en', 'es'].includes(langLocalStorage)) {
+		return langLocalStorage as Language
+	}
+
+	//3. Get Browser Language
+	const browserLang = navigator.language
+	if (browserLang.includes('es')) {
+		localStorage.setItem('language', 'es')
+		return 'es' as Language
+	}
+
+	//4. Default
+	return 'en' as Language
+}
+
 export const useLanguageStore = create<LanguageStore>((set) => ({
-	language: (localStorage.getItem('language') as Language) || 'en',
+	language: getInitialLanguage(),
 	setLanguage: (language) => {
 		localStorage.setItem('language', language)
 		set({ language })
