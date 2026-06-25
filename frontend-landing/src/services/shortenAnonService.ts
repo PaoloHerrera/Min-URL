c540b38bee1545d44e5817bfaa7d88076ef696aa
Paolo <paolo.herrera.araya@gmail.com>
2026-06-25 01:16:15 -0400

    fix: :bug: fix endpoint bug
    Update env endpoint and remove ShortenerForm props

--- frontend-landing/src/services/shortenAnonService.ts
+++ frontend-landing/src/services/shortenAnonService.ts
@@ -1,13 +1,17 @@
 import axios from 'axios'
 
+const backendUrl = import.meta.env.PUBLIC_BACKEND_URL || ''
+
 interface ShortenApiResponse {
 	shortUrl: string
 }
 
 export const shortenAnonService = async (longUrl: string): Promise<string> => {
 	const { data } = await axios.post<ShortenApiResponse>(
-		`${import.meta.env.VITE_SHORT_URL_DIRECT}/direct/shorten`,
-		{ originalUrl: longUrl },
+		`${backendUrl}/direct/shorten`,
+		{
+			originalUrl: longUrl,
+		},
 	)
 	return data.shortUrl
 }
