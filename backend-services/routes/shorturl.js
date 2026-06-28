ed5dd6ea1ef721a66a5438be980ae18985ccde83
Paolo <paolo.herrera.araya@gmail.com>
2026-06-27 20:16:45 -0400

    feat(backend): :sparkles: replace google recaptcha v3 with cloudflare turnstile middleware

--- backend-services/routes/shorturl.js
+++ backend-services/routes/shorturl.js
@@ -6,7 +6,7 @@ import { addGeolocation } from '../middleware/geolocationMiddleware.js'
 import { handleUrlErrors } from '../middleware/urlMiddlewares.js'
 import { validateUrl } from '../middleware/validateUrl.js'
 import { verifyGeoIp } from '../middleware/verifyIp.js'
-import { verifyRecaptcha } from '../middleware/verifyRecaptcha.js'
+import { verifyTurnstile } from '../middleware/verifyTurnstile.js'
 
 const routesShortUrl = Router()
 
@@ -14,7 +14,7 @@ routesShortUrl.get('/:slug', verifyGeoIp, getShortUrl)
 
 routesShortUrl.post(
 	'/direct/shorten',
-	verifyRecaptcha,
+	verifyTurnstile,
 	validateUrl,
 	checkForbiddenExtension,
 	addGeolocation,
