d285d3e14e910274e37503e88c5384329f6b7396
Paolo <paolo.herrera.araya@gmail.com>
2026-07-14 19:26:56 -0400

    test(backend): add env setup test configuration

---
+++ backend-redirector/__test__/setup.ts
@@ -0,0 +1,4 @@
+// Set environment variables for the test environment
+process.env.FRONTEND_URL = 'http://localhost:4321'
+process.env.BACKEND_API_URL = 'http://localhost:3001'
+process.env.INTERNAL_SECRET = 'test-secret'
