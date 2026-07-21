c3beb1bff3bd45fa1bf253999338b3c2b9ad5fa9
Paolo <paolo.herrera.araya@gmail.com>
2026-07-20 22:26:21 -0400

    refactor(core): :fire: remove legacy files

--- apps/backend-services/src/core/ports/UrlRepository.interface.ts
+++
@@ -1,9 +0,0 @@
-import type { ShortUrl } from '../domain/entities/ShortUrl.entity.ts'
-
-export interface UrlRepository {
-	save(shortUrl: ShortUrl): Promise<void>
-
-	isSlugAvailable: (slug: string) => Promise<boolean>
-
-	getUrlBySlug: (slug: string) => Promise<ShortUrl | null>
-}
