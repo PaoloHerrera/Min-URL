6429145c71b1ec9e5c01be54bc931a356a44658d
Paolo <paolo.herrera.araya@gmail.com>
2026-08-03 18:02:40 -0400

    feat(backend-services): implement HTTP errors and jsonPayloadLimit middleware
    Ref #37

--- apps/backend-services/src/adapters/primary/http/routes/shorturl.route.ts
+++ apps/backend-services/src/adapters/primary/http/routes/shorturl.route.ts
@@ -1,12 +1,14 @@
 import { urlController } from '@/bootstrap.ts'
 import { shortenAnonymousRequestSchema } from '@min-url/contracts/schemas'
 import { Router } from 'express'
+import { jsonPayloadLimit } from '../middlewares/jsonPayloadLimit.middleware.ts'
 import { validateBody } from '../middlewares/validateBody.middleware.ts'
 
 const routesShortUrl = Router()
 
 routesShortUrl.post(
 	'/direct/shorten',
+	jsonPayloadLimit(5),
 	validateBody(shortenAnonymousRequestSchema),
 	urlController.createAnonymous,
 )
